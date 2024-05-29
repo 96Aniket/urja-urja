e2d867cb809993c3bdc3dd8fcbc0bace623632de
Aniket <aniketsupekar2004@gmail.com>
2024-05-29 19:45:30 +0530

    done

--- src/Components/Cravsel/Cravsel.js
+++ src/Components/Cravsel/Cravsel.js
@@ -1,33 +1,29 @@
-import "bootstrap/dist/css/bootstrap.css"
-import One from './1.png'
-// import '../Home.css'
-import banner from './banner.png'
+import { useState } from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import One from './1.png';
+import Banner from './banner.png';
+import './Cravsel.css'
 
- function Carousel (){
-    return(
-      <>
-        <div id="carouselExampleFade" class="carousel slide carousel-fade">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src={One} class="d-block w-100" alt="..."/>
-    </div>
-    <div class="carousel-item">
-      <img src={banner} class="d-block w-100" alt="..."/>
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
-</>
+function ControlledCarousel() {
+  const [index, setIndex] = useState(0);
 
-    )
+  const handleSelect = (selectedIndex) => {
+    setIndex(selectedIndex);
+  };
+
+  return (
+    <Carousel activeIndex={index} onSelect={handleSelect}>
+      <Carousel.Item>
+        <img className='pic' src={One}/>
+        
+      </Carousel.Item>
+      <Carousel.Item>
+        <img className='pic' src={Banner}/>
+        
+      </Carousel.Item>
+      
+    </Carousel>
+  );
 }
 
-
-export default Carousel;
+export default ControlledCarousel;
