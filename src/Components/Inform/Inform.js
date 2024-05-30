816eb5687fb74b4be9f7a5b749edf7ca90dc16e1
Aniket <aniketsupekar2004@gmail.com>
2024-05-30 14:21:39 +0530

    done

--- src/Components/Inform/Inform.js
+++ src/Components/Inform/Inform.js
@@ -7,21 +7,21 @@ import D3 from './4907157.jpg'
 export  function Inform (){
     return(
         <div   style={ { backgroundImage: `url(${D3})`}}>
-        <p>WELCOME TO <br/>URJA GLOBAL LIMITED</p><hr/>
-        <h1>We at Urja are committed to providing world-class services to rapidly develop India focusing on rural electrification especially in hilly areas, forest regions, deserts, islands, far flung villages, unmanned locations and other areas which require reliable and uninterrupted power supply. With a strong design and manufacturing back ground UGL has developed themselves as a leading manufacturer of Solar Energy Systems to cater to almost the complete range of customer requirements</h1>
+        <p className='pp'>WELCOME TO <br/>URJA GLOBAL LIMITED</p><hr/>
+        <h1 className='hh'>We at Urja are committed to providing world-class services to rapidly develop India focusing on rural electrification especially in hilly areas, forest regions, deserts, islands, far flung villages, unmanned locations and other areas which require reliable and uninterrupted power supply. With a strong design and manufacturing back ground UGL has developed themselves as a leading manufacturer of Solar Energy Systems to cater to almost the complete range of customer requirements</h1>
         <button>Know More-</button>
-        <h2>#CONSERVE ENERGY                #CREATE ENERGY</h2>
+        <h2 className='hhh'>#CONSERVE ENERGY                #CREATE ENERGY</h2>
         <img src={battery} className='pic' alt="..."/>
         <br/>
         <img src={ttte} className='pics' alt="..."/>
-        <p>SERVICE NETWORK</p>
+        <p className='pp'>SERVICE NETWORK</p>
         <hr/>
-        <h1>A nation-wide network of around 2000 service professionals with tremendous experience</h1>
+        <h1 className='hh'>A nation-wide network of around 2000 service professionals with tremendous experience</h1>
     <div className='manecard'>
-      <div className='card'><p>38</p><h1>Dealer Network</h1></div>
-      <div className='card'><p>59</p><h1>Sales & Service Team</h1></div>
-      <div className='card'><p>22</p><h1>Distributor Network</h1></div>
-      <div className='card'><p>10 Mn</p><h1>Happy Customers</h1></div>
+      <div className='card'><p className='pp'>38</p><h1 className='hh'>Dealer Network</h1></div>
+      <div className='card'><p className='pp'>59</p><h1 className='hh'>Sales & Service Team</h1></div>
+      <div className='card'><p className='pp'>22</p><h1 className='hh'>Distributor Network</h1></div>
+      <div className='card'><p className='pp'>10 Mn</p><h1 className='hh'>Happy Customers</h1></div>
     </div>
     <div className='manecards'>
         <div><img src={D1} className='side' alt="..."/></div>
