8652e8666ce4b7eacfc3c60e0b6dc16632b7aba4
Aniket <aniketsupekar2004@gmail.com>
2024-05-31 10:17:38 +0530

    done

--- src/Components/Inform/Inform.js
+++ src/Components/Inform/Inform.js
@@ -9,7 +9,7 @@ export  function Inform (){
         <div   style={ { backgroundImage: `url(${D3})`}}>
         <p className='pp'>WELCOME TO <br/>URJA GLOBAL LIMITED</p><hr/>
         <h1 className='hh'>We at Urja are committed to providing world-class services to rapidly develop India focusing on rural electrification especially in hilly areas, forest regions, deserts, islands, far flung villages, unmanned locations and other areas which require reliable and uninterrupted power supply. With a strong design and manufacturing back ground UGL has developed themselves as a leading manufacturer of Solar Energy Systems to cater to almost the complete range of customer requirements</h1>
-        <button>Know More-</button>
+        <button className='butt'>Know More-</button>
         <h2 className='hhh'>#CONSERVE ENERGY                #CREATE ENERGY</h2>
         <img className='zr' src={battery}/>
         <br/>
