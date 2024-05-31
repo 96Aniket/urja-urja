94c3f104fc1988943055b13444515ed369083178
Shital Shinde <shitalshinde5150@gmail.com>
2024-05-31 07:16:18 +0530

    all code push

--- src/Components/Product/Products.js
+++ src/Components/Product/Products.js
@@ -1,57 +1,56 @@
 import React from 'react';
 import './Products.css';
-import 'bootstrap/dist/css/bootstrap.css' ; 
+import 'bootstrap/dist/css/bootstrap.css';
 
 export default function Scooter(props) {
   return (
     <>
-     <div className='card'>
+      <div className='new'>
 
-        <img className='img' src={props.scooterbanner} />
+        <img className='urja' src={props.scooterbanner} />
       </div>
 
-      <div className='H1'>
-      <div className='Head'>
-         <h1><b><u>E-Life</u></b></h1>
-
-</div>
-
-<div >
-<p  className='row'>
-   Urja Introducing the best electric scooter E-Life with
-      an unrivalled range up to 100Kms<br /> per charge. It features a sleek & modern design,
-      with lightweight and durable frame and is <br />
-      equipped with advance safety features,
-      robust waterproof & dust resistant battery, while <br />
-      the powerful motor provides a smooth & enjoyable ride.</p>
-      <br/> <br/>
-
- <button type='Know More' className='Btn'>Know More</button>
-<button type='Know More' className='Btn'>Book Now</button>   
-  </div>
-
-
-        < img className='scooter' src={props.scooter} />
+      <div className='h1'>
+        <div className='head'>
+          <h1><b><u>E-Life</u></b></h1>
+        
+        </div>
+        </div>
+     <div className='second'>
+      <div>
+        <p className='paragraph'> Urja Introducing the best electric scooter E-Life with
+          an unrivalled range up to 100Kms per charge. It features a sleek & modern design,
+          with lightweight and durable frame and is equipped with advance safety features, robust waterproof & dust resistant battery, while 
+          the powerful motor provides a smooth & enjoyable ride.</p>
+      
+        {/* <button type='Know More' className='newbtn'>Know More</button>
+<button type='Know More' className='newbtn'>Book Now</button>    */}
+    
       </div>
       <div>
-      < img className='scooty' src={props.bike} />
-    </div>
-    <div className='head'>
-         <h1><b><u>E-zess</u></b></h1>
-
-</div>
-<div>
+      <img className='newimg' src={props.scooty} />
+      </div>
+      </div>
+     
+        <div>
+        <img className='vehicle' src={props.bike}/>
+        </div>
+        <div className='point'>
+    <div className='Head'>
+      <h1><b><u>E-zess</u></b></h1>
+     </div>
+     <div >
 <p  className='col'>
    Urja Introducing the best electric scooter E-Life with
-      an unrivalled range up to 100Kms<br /> per charge. It features a sleek & modern design,
-      with lightweight and durable frame and is <br />
+      an unrivalled range up to 100Kms per charge. It features a sleek & modern design,
+      with lightweight and durable frame and is 
       equipped with advance safety features,
-      robust waterproof & dust resistant battery, while <br />
-      the powerful motor provides a smooth & enjoyable ride. <br/>The E-ZESS is here to change the way India moves.</p>
-      <button type='text' className='imge'>Know More</button>
-<button type='text' className='imge'>Book Now</button>
-  </div>
-
+      robust waterproof & dust resistant battery, while 
+      the powerful motor provides a smooth & enjoyable ride.The E-ZESS is here to change the way India moves.</p>
+    </div>
+    {/* <button type='text' className='imge'>Know More</button>
+<button type='text' className='imge'>Book Now</button> */}
+    </div>
     </>
 
   )
@@ -71,4 +70,4 @@ export default function Scooter(props) {
 
 
 
- 
+
