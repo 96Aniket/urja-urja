87887db3b29150bc4c84f62aaaf5f50d900e541f
Pooja Repale <poojarepale617@gmail.com>
2024-05-31 09:34:16 +0530

    change margin

--- src/View/Career/Career-Pooja.js
+++ src/View/Career/Career-Pooja.js
@@ -9,11 +9,12 @@ export function Career() {
         <>
         <Navbar/>
             <div className='body'>
+                
                 <img src={img} className='mainimg'></img>
                 <h1 className='heading'> Register with Us </h1>
                 <p className='line'>___________</p>
-                <p className='paragraph'> If you're interested in joining our team, please submit your resume through our website. We review applications on a rolling basis, and we'll be in touch if we  believe you're a good fit for <br /> one of our open positions. Thank you for considering a career with us. We look forward to hearing from you!</p>
-
+                <p className='parag'> If you're interested in joining our team, please submit your resume through our website. We review applications on a rolling basis, and we'll be in touch if we  believe you're a good fit for <br /> one of our open positions. Thank you for considering a career with us. We look forward to hearing from you!</p>
+                
                 <form className='form'>
                     <label>First Name</label><br />
                     <input type='text' placeholder='First Name' className='text-type'></input><br /><br />
@@ -25,11 +26,14 @@ export function Career() {
                     <input type='file' className='text-type'></input><br/>
 
                     <button className='btn'>SEND</button>
+
+                    
                     
                 </form>
-                
+                <br/><br/><br/><br/><br/>
 
             </div>
+            
             <Footer/>
         </>
     )
